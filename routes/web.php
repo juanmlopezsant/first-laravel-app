eebb9559f8594fb63dc4b502b95053e4fb583e83
Juan Manuel Lopez <juan.lopez@cceo.com.mx>
2020-03-26 16:28:39 -0600

    first controller created

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-$portfolio = [
-    ['title' => 'Proyecto 1'],
-    ['title' => 'Proyecto 2'],
-    ['title' => 'Proyecto 3'],
-    ['title' => 'Proyecto 4']
-
-]; 
-
-//Otra forma de hacerlo, un poco más óptimo: 
 Route::view('/', 'home')->name('home'); 
 Route::view ('/about', 'about')->name('about');
-Route::view ('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
+Route::get('/portfolio','PortfolioController')->name('portfolio'); 
 Route::view ('/contact', 'contact')->name('contact');
 
 
